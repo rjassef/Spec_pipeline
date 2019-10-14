dea99cfa75b0e8c8c984fbd3797f16dc63d86507
rjassef <roberto.assef@mail.udp.cl>
2019-10-14 17:48:27 -0300

    rjassef

---
+++ noise_fit_test/better_ESO_test_plot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+python better_ESO_test_plot.py 20
+python better_ESO_test_plot.py 21
+python better_ESO_test_plot.py 22
+python better_ESO_test_plot.py 23
+python better_ESO_test_plot.py 24
+python better_ESO_test_plot.py 25
