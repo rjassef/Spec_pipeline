dbf07c9bd75fad24ab8c0be8f1b91dc491ded320
rjassef <roberto.assef@mail.udp.cl>
2020-02-11 15:32:26 -0800

    Updated get_error. Numpy percentile no longer looses units.

--- Spec_pipeline/Line_Fitter/MC_errors_general.py
+++ Spec_pipeline/Line_Fitter/MC_errors_general.py
@@ -42,8 +42,8 @@ def plot_MC_chain(chain,bf_par, err_low, err_hig):
 
 
 def get_error(x,xbf,cf=68.3):
-    xlow = xbf - np.percentile(x[x<xbf], 100.-cf)*xbf.unit
-    xhig = np.percentile(x[x>xbf], cf)*xbf.unit - xbf
+    xlow = xbf - np.percentile(x[x<xbf], 100.-cf)
+    xhig = np.percentile(x[x>xbf], cf) - xbf
     return xlow, xhig
 
 
