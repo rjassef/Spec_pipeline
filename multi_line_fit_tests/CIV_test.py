8e3ebda1f97e41b101764b9e4f28bc8091eea88d
rjassef <roberto.assef@mail.udp.cl>
2020-02-11 14:32:10 -0800

    Modified to test new line_SNR property.

--- multi_line_fit_tests/CIV_test.py
+++ multi_line_fit_tests/CIV_test.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
-import warnings
-warnings.simplefilter("ignore")
-
-import numpy as np
-import astropy.units as u
+#import warnings
+#warnings.simplefilter("ignore")
 import multiprocessing as mp
-import copy
-import matplotlib.pyplot as plt
 
 from Spec_pipeline.Spec_Reader.read_spec import read_spec
 from Spec_pipeline.Line_Fitter.multi_line import Multi_Line_fit
@@ -18,6 +13,7 @@ x = cat.readline().split()
 spec = read_spec(x[0],float(x[1]),x[2],x[4:],grname=x[3])
 
 line_fit = Multi_Line_fit("CIV",spec=spec)
-
 line_fit.run_fit()
-line_fit.run_MC(100,Ncpu=7)
+line_fit.run_MC(100,Ncpu=mp.cpu_count())
+print(line_fit.print_MC_header)
+print(line_fit.print_MC)
