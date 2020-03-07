dc52d3b6a38805495a5ae148b2bf72e1797bd838
Roberto Assef <roberto.assef@mail.udp.cl>
2020-03-07 15:55:47 -0300

    Added main Spec_Reader and Line_Fitter module imports.

--- Spec_pipeline/__init__.py
+++ Spec_pipeline/__init__.py
@@ -1,1 +1,9 @@
 name = "Spec_pipeline"
+
+#First, write the standard Spec_Reader modules we'll be using.
+from .Spec_Reader.LRIS_Spec import LRIS_Spec
+from .Spec_Reader.GMOS_Spec import GMOS_Spec
+from .Spec_Reader.DBSP_Spec import DBSP_Spec
+
+#Now, from the Line_Fitter part, import only the multi_line class modules, the rest are deprecated.
+from .Line_Fitter.multi_line import Multi_Line_fit
