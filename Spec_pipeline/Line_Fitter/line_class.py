055433bba958372f7e8eae2902699063cc693291
Roberto Assef <roberto.assef@mail.udp.cl>
2020-04-30 11:11:34 -0400

    minor update.

--- Spec_pipeline/Line_Fitter/line_class.py
+++ Spec_pipeline/Line_Fitter/line_class.py
@@ -7,7 +7,6 @@ from scipy.special import betainc
 from astropy.constants import c
 
 from . import MC_errors_general as MC
-#from . import fit_general as fit
 from . import new_fit_general as fit
 from . import plot_fit
 
