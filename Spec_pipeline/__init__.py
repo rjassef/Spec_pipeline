939806d246ea7bdcc6a9ebe1226920b3ce70525f
rjassef <roberto.assef@mail.udp.cl>
2020-05-16 13:12:10 -0400

    Added SDSS_spec.

--- Spec_pipeline/__init__.py
+++ Spec_pipeline/__init__.py
@@ -4,6 +4,7 @@ name = "Spec_pipeline"
 from .Spec_Reader.LRIS_Spec import LRIS_Spec
 from .Spec_Reader.GMOS_Spec import GMOS_Spec
 from .Spec_Reader.DBSP_Spec import DBSP_Spec
+from .Spec_Reader.SDSS_Spec import SDSS_Spec
 
 #Now, from the Line_Fitter part, import only the multi_line class modules, the rest are deprecated.
 from .Line_Fitter.multi_line import Multi_Line_fit
