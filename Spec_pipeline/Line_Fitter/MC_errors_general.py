Fixed bug about units for failed ranges.

Spec_pipeline/Line_Fitter/MC_errors_general.py
@@ -46,11 +46,17 @@ def get_error(x,xbf,cf=68.3):
     try:
         xlow = xbf - np.percentile(x[x<xbf], 100.-cf)
     except IndexError:
-        xlow = 0.
+        try:
+            xlow = 0.*xbf.unit
+        except AttributeError:
+            xlow = 0.
     try:
         xhig = np.percentile(x[x>xbf], cf) - xbf
     except IndexError:
-        xhig = 0.
+        try:
+            xhig = 0.*xbf.unit
+        except AttributeError:
+            xhig = 0.
     return xlow, xhig
 
 