fa78f6effa66eda123a941a8645b6156aec77637
rjassef <roberto.assef@mail.udp.cl>
2020-09-15 16:08:19 -0300

    Updated sensitivity files for DBSP.

---
+++ Spec_pipeline/Sensitivity_Files/DBSP/transfer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Based on Google Doc to figure our configurations. Note that the format of the file name is sens_palomar_XXyyy_grating_dichroic_channel.fits, where XX is the year 20XX of the observations and yyy is the month.
+
+cp origs/sens_palomar_10sep_600_10000_d68_r.fits sens_DBSP_Site424_600-10000_D68_r.fits
+
+cp origs/sens_palomar_11aug_300_3990_d68_b.fits sens_DBSP_CCD44-82_300-3990_D68_b.fits
+cp origs/sens_palomar_11aug_316_7500_d68_r.fits sens_DBSP_Site424_316-7500_D68_r.fits
+
+cp origs/sens_palomar_12jul_300_3990_d55_b.fits sens_DBSP_CCD44-82_300-3990_D55_b.fits
+
+cp origs/sens_palomar_12aug_600_4000_d68_b.fits sens_DBSP_CCD44-82_600-4000_D68_b.fits
+cp origs/sens_palomar_12aug_600_10000_d68_r.fits sens_DBSP_LBNL_4Kx2K_600-10000_D68_r.fits
+
+cp origs/sens_palomar_13feb_600_4000_d55_b.fits sens_DBSP_CCD44-82_600-4000_D55_b.fits
+cp origs/sens_palomar_13feb_600_10000_d55_r.fits sens_DBSP_LBNL_4Kx2K_600-10000_D55_r.fits
+
+cp origs/sens_palomar_20aug_316_7500_d55_r.fits sens_DBSP_LBNL_4Kx2K_316-7500_D55_r.fits
