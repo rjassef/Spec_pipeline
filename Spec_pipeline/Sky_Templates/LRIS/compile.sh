089e647b22ee5e5340678e7b27a95cf9333ba58c
rjassef <roberto.assef@mail.udp.cl>
2020-09-17 14:51:48 -0300

    Update and cleanups of sky templates for LRIS.

---
+++ Spec_pipeline/Sky_Templates/LRIS/compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp sky_keck_b.w.txt template_sky_LRIS_400-3400_1.50arcsec_b.txt
+cp sky_keck_b.w.txt template_sky_LRIS_600-4000_1.50arcsec_b.txt
+cp sky_keck_r.w.txt template_sky_LRIS_400-8500_1.50arcsec_r.txt
