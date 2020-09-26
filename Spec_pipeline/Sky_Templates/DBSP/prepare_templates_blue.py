Updated sky templates. Bug fixed about units.

Spec_pipeline/Sky_Templates/DBSP/prepare_templates_blue.py
@@ -10,7 +10,7 @@ from Spec_pipeline.Spec_Reader.iraf_spectrum1d import read_fits_spectrum1d
 
 #Array that holds all information about the gratings. Resolutions in angstroms/mm.
 res_page = dict()
-#res_page['158-7560']  = [135.,201.]
+res_page['158-7560']  = [135.,201.]
 res_page['300-3990']  = [140.,'-' ]
 res_page['316-7500']  = ['-' ,102.]
 res_page['600-4000']  =	[ 71., '-']
@@ -20,10 +20,13 @@ res_page['600-10000'] =	['-' , 54.]
 #res_page['1200-9400'] =	[ 35., 26.]
 
 #Set the scale of arcseconds to microns. This should be independent of the detector, but we can easilty get it from the detector parameters combining the pixel size in microns with the plate_scale (arcsec/pixel).
-scale = 0.293*u.arcsec / (15.*u.micron)
+scale = 0.389*u.arcsec / (15.*u.micron)
 
 #Read the blue template.
-spec = read_fits_spectrum1d("sky_palomar_r.w.fits")
+spec = read_fits_spectrum1d("sky_palomar_D68_b.w.fits")
+spec_temp = np.loadtxt("template_sky_DBSP_D68_b.dat")
+lam_temp = spec_temp[:,0]*u.AA
+flam_temp = spec_temp[:,1] #*u.erg/u.s/u.cm**2/u.AA
 
 #Get the aperture of the template
 slit_width_temp = float(spec[0].header['APERTURE']) * u.arcsec
@@ -36,7 +39,7 @@ if grating_temp == "3167500":
     grating_temp = "316-7500"
 
 #Get the template grating resolution
-res_temp = res_page[grating_temp][1] * u.angstrom/u.mm
+res_temp = res_page[grating_temp][0] * u.angstrom/u.mm
 
 #Set the template resolution
 FWHM_res_temp  = (slit_width_temp/scale) * res_temp
@@ -47,11 +50,11 @@ slit_widths = np.array([1.5, 2.0])*u.arcsec
 for slit_width in slit_widths:
     for grating in res_page.keys():
 
-        if res_page[grating][1]=='-':
+        if res_page[grating][0]=='-':
             continue
 
         #Get the template grating resolution
-        res = res_page[grating][1] * u.angstrom/u.mm
+        res = res_page[grating][0] * u.angstrom/u.mm
 
         #Set the resolution to use
         FWHM_res  = (slit_width/scale) * res
@@ -61,19 +64,24 @@ for slit_width in slit_widths:
         if sigma_res>sigma_res_temp:
             print("Creating template for {0:s} grating, {1:s} slit width".format(grating, slit_width))
             sigma_use = (sigma_res**2-sigma_res_temp**2)**0.5
-            bin_size = np.mean(spec[0].dispersion[1:]-spec[0].dispersion[:-1])
+            #bin_size = np.mean(spec[0].dispersion[1:]-spec[0].dispersion[:-1])
+            bin_size = np.mean(lam_temp[1:]-lam_temp[:-1])
             sigma_use_pix = (sigma_use/bin_size).to(1.).value
             gauss_kernel = Gaussian1DKernel(stddev=sigma_use_pix)
-            sky_conv = convolve(spec[0].data, gauss_kernel)
+            #sky_conv = convolve(spec[0].data, gauss_kernel)
+            sky_conv = convolve(flam_temp, gauss_kernel)
         else:
             print("Warning. Template resolution is worse than requested resolution. Using unmodified template.")
             print("Template grating: {0:s}  Template aperture: {1:f}".format(grating_temp, slit_width_temp))
             print("Requested grating: {0:s} Requested aperture: {1:f}".format(grating, slit_width))
-            sky_conv = spec[0].data
+            #sky_conv = spec[0].data
+            sky_conv = flam_temp
 
         #Write the resulting sky model.
-        fname = "template_sky_DBSP_{0:s}_{1:.2f}arcsec_r.txt".format(grating, slit_width.to(u.arcsec).value)
-        np.savetxt(fname,np.array([spec[0].dispersion, sky_conv]).T)
-        plt.plot(spec[0].dispersion, sky_conv,'-b')
+        fname = "template_sky_DBSP_{0:s}_{1:.2f}arcsec_b.txt".format(grating, slit_width.to(u.arcsec).value)
+        #np.savetxt(fname,np.array([spec[0].dispersion, sky_conv]).T)
+        #plt.plot(spec[0].dispersion, sky_conv,'-b')
+        np.savetxt(fname,np.array([lam_temp, sky_conv]).T)
+        plt.plot(lam_temp, sky_conv,'-b')
         plt.savefig(re.sub(".txt",".png",fname))
         plt.close()