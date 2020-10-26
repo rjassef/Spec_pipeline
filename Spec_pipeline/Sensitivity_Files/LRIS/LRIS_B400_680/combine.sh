989576341fd8eedd9fe5cc460160d3feb9fe75c3
rjassef <roberto.assef@mail.udp.cl>
2020-10-26 18:43:02 -0300

    Cleanup and updates on sensitivity curves.

---
+++ Spec_pipeline/Sensitivity_Files/LRIS/LRIS_B400_680/combine.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python ../combine.py Sens_LRIS_e2v_400-3400_680_b 15 4750 *.fits
--- Spec_pipeline/Sensitivity_Files/LRIS/combine.py
+++ Spec_pipeline/Sensitivity_Files/LRIS/combine.py
@@ -76,38 +76,48 @@ lmin = np.max([lmin.to(u.AA).value,3000.])*u.AA
 lmax = np.min([lmax.to(u.AA).value,11000.])*u.AA
 
 #Put them in the same wavelength grid.
-wave = np.arange(np.min(lmin).value,np.max(lmax).value,0.5)*u.AA
-sens_resampled = ma.zeros((len(sens),len(wave)))
-for i,s in enumerate(sens):
-    sens_use = ma.masked_where(s.data<=0.,s.data)
-    sens_resampled[i] = rebin_sens(s.dispersion, sens_use, wave)
+if len(sens)>1:
+    wave = np.arange(np.min(lmin).value,np.max(lmax).value,0.5)*u.AA
+    sens_resampled = ma.zeros((len(sens),len(wave)))
+    for i,s in enumerate(sens):
+        sens_use = ma.masked_where(s.data<=0.,s.data)
+        sens_resampled[i] = rebin_sens(s.dispersion, sens_use, wave)
 
-#Normalize all the curves at 4750A or 8000A, depending on which is observed.
-# if lmin<4750.*u.AA:
-#     lam_norm = 4750.*u.AA
-# else:
-#     lam_norm = 8000.*u.AA
-ktarg = np.argmin(np.abs(wave.to(u.AA).value-lam_norm.to(u.AA).value))
-for i in range(1,len(sens)):
-    norm = sens_resampled[0][ktarg]/sens_resampled[i][ktarg]
-    sens_resampled[i] *= norm
+    #Normalize all the curves at 4750A or 8000A, depending on which is observed.
+    # if lmin<4750.*u.AA:
+    #     lam_norm = 4750.*u.AA
+    # else:
+    #     lam_norm = 8000.*u.AA
+    ktarg = np.argmin(np.abs(wave.to(u.AA).value-lam_norm.to(u.AA).value))
+    for i in range(1,len(sens)):
+        norm = sens_resampled[0][ktarg]/sens_resampled[i][ktarg]
+        sens_resampled[i] *= norm
 
-#Calculate the median.
-median_sens = ma.median(sens_resampled,axis=0)
+    #Calculate the median.
+    median_sens = ma.median(sens_resampled,axis=0)
 
-#Fit the median to obtain an smooth curve.
-p = np.polyfit(wave,median_sens,polyfit_degree)
-fit_sens = np.poly1d(p)
+    #Fit the median to obtain an smooth curve.
+    p = np.polyfit(wave,median_sens,polyfit_degree)
+    fit_sens_func = np.poly1d(p)
+    fit_sens = fit_sens_func(wave.value)
+
+    #Plot all the curves an the median.
+    for s in sens_resampled:
+        plt.plot(wave,s,'--')
+
+else:
+    wave = sens[0].dispersion
+    fit_sens = sens[0].data
 
 #Name of the combined sensitivity curve.
 #fname = "Sens_LRIS_e2v_600-4000_560_b"
 
 #Plot all the curves an the median.
-for s in sens_resampled:
-    plt.plot(wave,s,'--')
-plt.plot(wave,fit_sens(wave.value),'b-')
+#for s in sens_resampled:
+#    plt.plot(wave,s,'--')
+plt.plot(wave,fit_sens,'b-')
 #plt.show()
 plt.savefig(fname+".png")
 
 #Save the final curve.
-np.savetxt(fname+".txt",np.array([wave.value, fit_sens(wave.value)*0.01]).T)
+np.savetxt(fname+".txt",np.array([wave.value, fit_sens*0.01]).T)
